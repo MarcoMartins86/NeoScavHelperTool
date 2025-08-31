df5d9c1305efe30c6ae627c8b702a20bfed05b31
Marco Martins <marcomartins86@gmail.com>
2025-08-31 01:52:03 +0100

    initial serilog addition

--- NeoScavHelperTool/App.xaml.cs
+++ NeoScavHelperTool/App.xaml.cs
@@ -2,13 +2,16 @@
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using NeoScavHelperTool.Services;
 using NeoScavHelperTool.ViewModels;
 using NeoScavHelperTool.Views;
+using Serilog;
 
 namespace NeoScavHelperTool
 {
@@ -33,7 +36,21 @@ namespace NeoScavHelperTool
         private void ConfigureServices(IServiceCollection services)
         {
             // Configure Logging
-            services.AddLogging();
+            File.Delete("./last_run.txt");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Async(a =>
+                    a.File(
+                        "./last_run.txt",
+                        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} <{ThreadId}> [{Level:u3}] {Message:lj}{NewLine}{Exception}"
+                    )
+                )
+                .Enrich.WithThreadId()
+                .CreateLogger();
+
+            services.AddLogging(builder =>
+                builder.SetMinimumLevel(LogLevel.Trace).ClearProviders().AddSerilog(dispose: true)
+            );
 
             // Register Services
             services.AddSingleton(Current.Dispatcher);

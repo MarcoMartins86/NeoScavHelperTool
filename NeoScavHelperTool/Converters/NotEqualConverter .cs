7dc76ef733e25fe95604754b705f071ef1df8c60
Marco Martins <marcomartins86@gmail.com>
2025-10-18 00:40:37 +0100

    improved ui

---
+++ NeoScavHelperTool/Converters/NotEqualConverter .cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NeoScavHelperTool.Converters
+{
+    public class NotEqualConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter != null)
+            {
+                return !Equals(value, System.Convert.ChangeType(parameter, value.GetType()));
+            }
+            else if (value == null)
+            {
+                return true;
+            }
+            throw new ArgumentNullException("The parameter \"parameter\" cannot be null");
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture
+        )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
